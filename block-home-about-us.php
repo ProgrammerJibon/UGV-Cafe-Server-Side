43e1520c420d2862a1a4d97dfbad1c593ea36f7b
MD Jibon Howlader <ProgrammerJibon@gmail.com>
2022-03-19 22:58:18 +0600

    19-Mar-22; 10:58PM;

--- block-home-about-us.php
+++ block-home-about-us.php
@@ -6,7 +6,7 @@
     <div class="title">
         <span>who are we</span>
     </div>
-    <div class="parallax-window" style="height: 800px;" data-parallax="scroll" data-z-index="1" data-image-src="cdn/32fc87_90073a4888974e63b430f31471187745_mv2.jpg"></div>
+    <div class="parallax-window" style="height: 800px;" data-parallax="scroll" data-z-index="1" data-image-src="<?php echo $info['home-block-bg-2']; ?>"></div>
     <div class="desc">
         <span>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aperiam officiis eum ipsum, aliquid nemo. Consequuntur odio sint quia magnam necessitatibus. Laboriosam officiis veritatis, quidem dignissimos magni veniam beatae sunt?
--- block-home-banner.php
+++ block-home-banner.php
@@ -1,5 +1,5 @@
 <div class="container" id="welcome-screen">
-    <div class="parallax-window" style="height: 800px;" data-parallax="scroll" data-z-index="1" data-image-src="cdn/32fc87_1039a9f6b3c14fb69dac80d04c1cf578_mv2.jpg">
+    <div class="parallax-window" style="height: 800px;" data-parallax="scroll" data-z-index="1" data-image-src="<?php echo $info['home-block-bg-1']; ?>">
         <div class="parallax-window-inset" style="height: 800px;">
             <div class="est-date">
                 <span>EST. | <?php echo $info['est']; ?></span>
